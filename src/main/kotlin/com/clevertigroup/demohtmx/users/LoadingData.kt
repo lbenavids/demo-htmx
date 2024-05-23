b9d1d208905bba3ea74e00c503316e7ff189e078
Luis Benavides <luis.benavides@collectivehealth.com>
2024-05-23 13:03:06 -0400

    Update user list template and user data generation
    The list layout in the user list template has been updated from a simple unordered list to a more detailed definition list that includes both the name and username. LoadingData.kt now generates 50 random users with randomly generated strings for names and usernames. Removed redundant repository parameter in UserController.

--- src/main/kotlin/com/clevertigroup/demohtmx/users/LoadingData.kt
+++ src/main/kotlin/com/clevertigroup/demohtmx/users/LoadingData.kt
@@ -2,11 +2,25 @@ package com.clevertigroup.demohtmx.users
 
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
+import kotlin.random.Random
 
 @Component
 class LoadingData(val repository: UserRepository) : CommandLineRunner{
+    private fun getRandomString(length: Int) : String {
+        val allowedChars = ('a'..'z') + ('A'..'Z')
+        return (1..length)
+            .map { Random.nextInt(0, allowedChars.size) }
+            .map(allowedChars::get)
+            .joinToString("")
+    }
+
     override fun run(vararg args: String?) {
-        val users = listOf("abc", "def").map { username -> User(name = username, username = username) }
+        repository.deleteAll()
+
+        // Generating a list of 5 random users
+        val users = (1..50).map { User(name = getRandomString(Random.nextInt(0,10)), username = getRandomString(Random.nextInt(0,10))) }
+
+        // Saving all the users
         repository.saveAll(users)
     }
 }
--- src/main/kotlin/com/clevertigroup/demohtmx/users/UserController.kt
+++ src/main/kotlin/com/clevertigroup/demohtmx/users/UserController.kt
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping
 @RequestMapping("/users")
 class UserController(
     private val userRepository: UserRepository,
-    repository: UserRepository
 ) {
 
     @GetMapping
